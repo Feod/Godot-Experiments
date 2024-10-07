017d6fdd03fe745ef25792b38ff0a4f661aa5364
Feod <miikkaharjuntausta@gmail.com>
2024-10-07 14:19:28 +0300

    New main scene

---
+++ PhysicsMove.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var physBodyTarget : RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		
+	velocity = velocity.normalized() * 100
+	
+	physBodyTarget.apply_impulse(velocity)
+	
+	
