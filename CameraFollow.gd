98e0a748e885a84807165062b54d3a699ea4bd08
Feod <miikkaharjuntausta@gmail.com>
2024-10-07 14:21:52 +0300

    Add a `Camera2D` node to `WorldScene2D` and implement camera follow script
    * Add a `Camera2D` node as a child of `WorldScene2D` and attach `CameraFollow.gd` script
    * Create `CameraFollow.gd` script to handle following all `RigidBody2D` nodes
    * In `_ready()`, get all `RigidBody2D` nodes in the scene
    * In `_process(delta)`, calculate the average position of all `RigidBody2D` nodes and set the camera's position to the calculated average position

---
+++ CameraFollow.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+var rigid_bodies = []
+
+func _ready():
+    rigid_bodies = get_tree().get_nodes_in_group("RigidBodies")
+
+func _process(delta):
+    if rigid_bodies.size() == 0:
+        return
+
+    var average_position = Vector2()
+    for body in rigid_bodies:
+        average_position += body.global_position
+    average_position /= rigid_bodies.size()
+
+    global_position = average_position
