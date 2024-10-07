5f82ec9db56fcd620077f420ea32359549014f4a
Feod <miikkaharjuntausta@gmail.com>
2024-10-07 14:33:00 +0300

    Edits

--- CameraFollow.gd
+++ CameraFollow.gd
@@ -3,15 +3,15 @@ extends Camera2D
 var rigid_bodies = []
 
 func _ready():
-    rigid_bodies = get_tree().get_nodes_in_group("RigidBodies")
+	rigid_bodies = get_tree().get_nodes_in_group("RigidBodies")
 
 func _process(delta):
-    if rigid_bodies.size() == 0:
-        return
+	if rigid_bodies.size() == 0:
+		return
 
-    var average_position = Vector2()
-    for body in rigid_bodies:
-        average_position += body.global_position
-    average_position /= rigid_bodies.size()
+	var average_position = Vector2()
+	for body in rigid_bodies:
+		average_position += body.global_position
+	average_position /= rigid_bodies.size()
 
-    global_position = average_position
+	global_position = average_position
