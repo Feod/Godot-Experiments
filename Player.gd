573be08903104ea750ca5d75eeac1a789f568827
Feod <miikkaharjuntausta@gmail.com>
2024-10-07 13:45:50 +0300

    Add player character script and integrate into main scene
    * **Player.gd**
      - Create a new GDScript file for the player character
      - Define a class `Player` extending `KinematicBody2D`
      - Implement `_process(delta)` function to handle movement using Arrows/WASD keys
      - Add movement speed variable and logic to move the character
    
    * **MainScene.tscn**
      - Add a `KinematicBody2D` node named `Player` to the scene
      - Attach the `Player.gd` script to the `Player` node
      - Add a `Sprite` node as a child of the `Player` node for visual representation

---
+++ Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+class_name Player
+
+var speed = 200
+
+func _process(delta):
+    var velocity = Vector2()
+    
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+
+    velocity = velocity.normalized() * speed
+    move_and_slide(velocity)
